c87359bc25adc70258b54f32ffb87fced1c0dce8
Ninjamaniac <40552433+Ninjamaniac@users.noreply.github.com>
2018-06-24 19:07:01 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.voice_client import VoiceClient
+import asyncio
+
+bot = commands.Bot(command_prefix=">")  
+
+@bot.event
+async def on_ready():
+    print ("Ready")
+    print (bot.user.name + " is online")
+    print (bot.user.id)
+
+@bot.command(pass_context=True)
+async def Father(ctx):
+   await bot.say('Praise be thy Noodle', tts=True)
+
+@bot.command(pass_context=True)
+async def Pray(ctx):
+    voice = await bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+    player = voice.create_ffmpeg_player('ourpasta.mp3', after=lambda: print('done'))
+    player.start()
+
+@bot.command(pass_context=True)
+async def Kevin(ctx):
+    voice = await bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+    player = voice.create_ffmpeg_player('kevin.mp3')
+    player.start()
+
+@bot.command(pass_context=True)
+async def Dismissed(ctx):
+    for x in bot.voice_clients:
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
+bot.login (process.env.TOKEN)
